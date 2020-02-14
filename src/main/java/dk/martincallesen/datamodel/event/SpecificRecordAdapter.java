04862c37049ee88335eb6d315d3075f55b32b84c
martincallesen <mail@martincallesen.dk>
2020-02-14 08:46:02 +0100

    created adapter for avro to contain the dependency

---
+++ src/main/java/dk/martincallesen/datamodel/event/SpecificRecordAdapter.java
@@ -0,0 +1,28 @@
+package dk.martincallesen.datamodel.event;
+
+import org.apache.avro.Schema;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.avro.specific.SpecificRecordBase;
+
+public class SpecificRecordAdapter extends SpecificRecordBase {
+    private SpecificRecord specificRecord;
+
+    public SpecificRecordAdapter(SpecificRecord specificRecord) {
+        this.specificRecord = specificRecord;
+    }
+
+    @Override
+    public void put(int i, Object o) {
+        specificRecord.put(i, 0);
+    }
+
+    @Override
+    public Object get(int i) {
+        return specificRecord.get(i);
+    }
+
+    @Override
+    public Schema getSchema() {
+        return specificRecord.getSchema();
+    }
+}
